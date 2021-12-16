e8bf2db613615897cdb59bb1099d033def10ecf9
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2021-12-15 19:24:16 -0500

    Delete generate.go

--- generate.go
+++
@@ -1,7 +0,0 @@
-//go:build gen
-
-//go:generate glow generate -out=./v3.2-core/gl/ -api=gl -version=3.2 -profile=core -xml=../glow/xml/ -tmpl=../glow/tmpl/
-
-// This is an empty pseudo-package with the sole purpose of containing go generate directives
-// that generate all gl binding packages inside this repository.
-package gl
